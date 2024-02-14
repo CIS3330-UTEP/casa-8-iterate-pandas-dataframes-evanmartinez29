ea1b37566f8b34d08cd1aec81b8e776c5ed649f0
evanmartinez29 <157407999+evanmartinez29@users.noreply.github.com>
2024-02-14 00:59:38 -0700

    Update casa_8 vs1.py
    Iterate over row for DataFrame big mac using methods iterrows() and apply()

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+df = pd.read_csv("big-mac-full-index.csv")
+
+
+print("Given DataFrame :\n", df)
+print("\nIterating over rows using iterrows() method :\n")
+
+for index, row in df.iterrows():
+    print(row["name"], row["dollar_price"], row["local_price"])
+
+
+print("Given Dataframe :\n", df)
+print("\nIterating over rows using apply function :\n")
+
+print(df.apply(lambda row: row['name'] + " " +
+               str(row["dollar_price"]) + " " + 
+               str(row["currency_code"]), axis=1))
